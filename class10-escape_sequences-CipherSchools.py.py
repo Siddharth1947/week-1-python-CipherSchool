adebb31a66ea92f1e6379845128d63d7400d49e8
Siddharth1947 <119068884+Siddharth1947@users.noreply.github.com>
2022-12-02 00:04:32 +0530

    Add files via upload

---
+++ class10-escape_sequences-CipherSchools.py.py
@@ -0,0 +1,3 @@
+print(' I\'m Radha Krishna Patnaik ')
+print('Be it \\ dfg')
+print("git bash \"")
---
+++ class11-comments in python-CipherSchools.py.py
@@ -0,0 +1,3 @@
+print(' I\'m Radha Krishna Patnaik ') # \' is for '
+print('Be it \\ dfg') # \\ is for \
+print("git bash \"") # \" to print "
